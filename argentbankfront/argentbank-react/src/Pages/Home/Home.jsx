f8d88a1c87278bc6e6609b7d363a2bc646d27001
LouisMichelin <louis.michelin097@gmail.com>
2024-05-02 21:30:12 +0200

    Home : Avancement

--- argentbankfront/argentbank-react/src/Pages/Home/Home.jsx
+++ argentbankfront/argentbank-react/src/Pages/Home/Home.jsx
@@ -1,10 +1,28 @@
 import "./Home.scss";
-// import BankTree from "../../../public/img/bank-tree.jpeg";
+import BankTree from "../../../public/img/bank-tree.jpeg";
 
 function Home() {
    return (
       <>
-         <div className="Home">HOME PAGE</div>
+         <main className="Home">
+            <section className="HomeSection1">
+               {/* <div className="HomeSection1-ImgContainer"> */}
+               <img className="HomeSection1-Img" src={BankTree} alt="" />
+               {/* </div> */}
+               <div className="HomeSection1-Description">
+                  {/* <p>Promoted Content</p> */}
+                  <p>No fees.</p>
+                  <p>No minimum deposit.</p>
+                  <p>High interest rates.</p>
+                  <p>Open a savings account with Argent Bank today!</p>
+               </div>
+            </section>
+            <section className="HomeSection2">
+               <div>Element 1</div>
+               <div>Element 2</div>
+               <div>Element 3</div>
+            </section>
+         </main>
          {/* <img src={BankTree} alt="" /> */}
       </>
    );
