158234fab0c9990233befe53232ce008d63212eb
LouisMichelin <louis.michelin097@gmail.com>
2024-05-08 17:47:27 +0200

    <Home/> : Section Hero : 100% OK

--- argentbankfront/argentbank-react/src/Pages/Home/Home.jsx
+++ argentbankfront/argentbank-react/src/Pages/Home/Home.jsx
@@ -6,18 +6,21 @@ function Home() {
       <>
          <main className="Home">
             <section className="HomeHero">
-               <div className="HomeHeroDescription">
-                  <p>No fees.</p>
-                  <p>No minimum deposit.</p>
-                  <p>High interest rates.</p>
-                  <p>Open a savings account with Argent Bank today!</p>
+               <div className="HomeHero-Content">
+                  <h2 className="sr-only">Promoted Content</h2>
+                  <p className="HomeHero-Subtitle">No fees.</p>
+                  <p className="HomeHero-Subtitle">No minimum deposit.</p>
+                  <p className="HomeHero-Subtitle">High interest rates.</p>
+                  <p className="HomeHero-Text">
+                     Open a savings account with Argent Bank today!
+                  </p>
                </div>
             </section>
-            <section className="HomeFeatures">
+            {/* <section className="HomeFeatures">
                <div>Element 1</div>
                <div>Element 2</div>
                <div>Element 3</div>
-            </section>
+            </section> */}
          </main>
          {/* <img src={BankTree} alt="" /> */}
       </>
