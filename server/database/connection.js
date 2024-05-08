aa1d422fa6713845d3e986b7ecb4ab95ccab40f7
LouisMichelin <louis.michelin097@gmail.com>
2024-05-08 15:40:05 +0200

    Connection.js : edit "localhost" to "127.0.0.1" for MongoDB to work

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,13 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+   //   process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+   process.env.DATABASE_URL || "mongodb://127.0.0.1/argentBankDB";
 
 module.exports = async () => {
-  try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
-    console.log('Database successfully connected')
-  } catch (error) {
-    console.error(`Database Connectivity Error: ${error}`)
-    throw new Error(error)
-  }
-}
+   try {
+      await mongoose.connect(databaseUrl, { useNewUrlParser: true });
+      console.log("Database successfully connected");
+   } catch (error) {
+      console.error(`Database Connectivity Error: ${error}`);
+      throw new Error(error);
+   }
+};
