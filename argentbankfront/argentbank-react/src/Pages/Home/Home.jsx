60219d5facf6adefef07158d56368b8d2323151c
LouisMichelin <louis.michelin097@gmail.com>
2024-05-08 18:55:18 +0200

    <Home/> : Page 100% OK

--- argentbankfront/argentbank-react/src/Pages/Home/Home.jsx
+++ argentbankfront/argentbank-react/src/Pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import "./Home.scss";
-// import BankTree from "../../../public/img/bank-tree.jpeg";
+import IconChat from "../../../public/img/icon-chat.png";
+import IconMoney from "../../../public/img/icon-money.png";
+import IconSecurity from "../../../public/img/icon-security.png";
 
 function Home() {
    return (
@@ -16,13 +18,51 @@ function Home() {
                   </p>
                </div>
             </section>
-            {/* <section className="HomeFeatures">
-               <div>Element 1</div>
-               <div>Element 2</div>
-               <div>Element 3</div>
-            </section> */}
+            <section className="HomeFeatures">
+               <h2 className="sr-only">Features</h2>
+               <div className="HomeFeatures-Item">
+                  <img
+                     className="HomeFeatures-Icon"
+                     src={IconChat}
+                     alt="Chat Icon"
+                  />
+                  <h3 className="HomeFeatures-Title">
+                     You are our #1 priority
+                  </h3>
+                  <p>
+                     Need to talk to a representative? You can get in touch
+                     through our 24/7 chat or through a phone call in less than
+                     5 minutes.
+                  </p>
+               </div>
+               <div className="HomeFeatures-Item">
+                  <img
+                     className="HomeFeatures-Icon"
+                     src={IconMoney}
+                     alt="Chat Icon"
+                  />
+                  <h3 className="HomeFeatures-Title">
+                     More savings means higher rates
+                  </h3>
+                  <p>
+                     The more you save with us, the higher your interest rate
+                     will be!
+                  </p>
+               </div>
+               <div className="HomeFeatures-Item">
+                  <img
+                     className="HomeFeatures-Icon"
+                     src={IconSecurity}
+                     alt="Chat Icon"
+                  />
+                  <h3 className="HomeFeatures-Title">Security you can trust</h3>
+                  <p>
+                     We use top of the line encryption to make sure your data
+                     and money is always safe.
+                  </p>
+               </div>
+            </section>
          </main>
-         {/* <img src={BankTree} alt="" /> */}
       </>
    );
 }
