02851b63150aadb96b240af81839a89b576136d5
LouisMichelin <louis.michelin097@gmail.com>
2024-05-12 18:42:56 +0200

    <User /> : 100% OK

--- argentbank-react/src/Pages/User/User.jsx
+++ argentbank-react/src/Pages/User/User.jsx
@@ -5,39 +5,41 @@ function User() {
       <>
          <main className="User">
             <div className="User-Header">
-               <h1>Hello World</h1>
-               <button>Click Me LOL</button>
+               <h1>
+                  Welcome back <br /> Tony Jarvis!
+               </h1>
+               <button className="User-EditButton">Edit Name</button>
             </div>
             <h2 className="sr-only">Accounts</h2>
 
             <section className="UserAccount">
                <div className="UserAccount-ContentWrapper">
-                  <h3>Argent Bank Checking (à éditer...)</h3>
-                  <p>Argent (à éditer...)</p>
+                  <h3>Argent Bank Checking (x8349)</h3>
+                  <p className="UserAccount-Amount">$2,082.79</p>
                   <p>Available Balance</p>
                </div>
                <div className="UserAccount-Button">
-                  <button>CLIQUEZ ICI</button>
+                  <button>View transactions</button>
                </div>
             </section>
             <section className="UserAccount">
                <div className="UserAccount-ContentWrapper">
-                  <h3>Argent Bank Checking (à éditer...)</h3>
-                  <p>Argent (à éditer...)</p>
+                  <h3>Argent Bank Savings (x6712)</h3>
+                  <p className="UserAccount-Amount">$10,928.42</p>
                   <p>Available Balance</p>
                </div>
                <div className="UserAccount-Button">
-                  <button>CLIQUEZ ICI</button>
+                  <button>View transactions</button>
                </div>
             </section>
             <section className="UserAccount">
                <div className="UserAccount-ContentWrapper">
-                  <h3>Argent Bank Checking (à éditer...)</h3>
-                  <p>Argent (à éditer...)</p>
-                  <p>Available Balance</p>
+                  <h3>Argent Bank Credit Card (x8349)</h3>
+                  <p className="UserAccount-Amount">$184.30</p>
+                  <p>Current Balance</p>
                </div>
                <div className="UserAccount-Button">
-                  <button>CLIQUEZ ICI</button>
+                  <button>View transactions</button>
                </div>
             </section>
          </main>
